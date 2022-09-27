7a59f4e71a820a10fc7cef32501d2be3b9760bf5
SailorStarHealer <108467609+SailorStarHealer@users.noreply.github.com>
2022-09-27 12:13:55 +0200

    Algorithms

---
+++ Algorithm 3/Algo3/Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+class Program
+
+{
+    static string OrdinalNumber(int number)
+
+    {
+
+        //Get the last digit of an integer.
+        int lastDigit = number % 10;
+
+        //If the number is bigger than 10, also get the second to last digit.
+        if (number > 10)
+        {
+            int numberSansLastDigit = number / 10;
+
+            int secondToLastDigit = numberSansLastDigit % 10;
+
+            //If the second to last digit is 1, return the integer plus "th"
+            if (secondToLastDigit == 1)
+            {
+
+                return $"{number}th";
+
+            }
+
+        }
+
+        //If the last digit is 1, return the integer plus "st".
+        if (lastDigit == 1)
+        {
+            return $"{number}st";
+        }
+
+        //If the last digit is 2, return the integer plus "nd".
+        if (lastDigit == 2)
+        {
+            return $"{number}nd";
+        }
+
+        //If the last digit is 3, return the integer plus "rd".
+        if (lastDigit == 3)
+        {
+            return $"{number}rd";
+        }
+
+        //Otherwise return integer plus "th".
+        return ($"{number}th");
+    }
+
+
+    static void Main(string[] args)
+    {
+        for (int inputNumber = 0; inputNumber < 200; inputNumber++)
+        {
+            Console.WriteLine($"{OrdinalNumber(inputNumber)}");
+
+        }
+
+
+
+    }
+
+}
